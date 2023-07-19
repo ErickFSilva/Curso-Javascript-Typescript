acf80f830963162c28253216cfe343dcedde2fee
ErickFSilva <erick_ferreira@outlook.com>
2023-07-19 16:50:59 -0300

    secao-02-javascript-basico, 19/07/2023 (tarde)

--- secao-02_javascript_basico/aula032-objetos.js
+++ secao-02_javascript_basico/aula032-objetos.js
@@ -1,8 +1,55 @@
+// - Array:
 const array = [1, 2, 3];
 
 array.push(4);
 array[0] = 'Luiz';
 console.log(array);
+console.log();
 
-array = 'Outra';
-console.log(array);
+// array = 'Outra'; - Retornaria um erro
+
+
+// - Objeto literal:
+const pessoa1 = {
+
+    // Atributos
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25
+};
+
+console.log(pessoa1.nome);
+console.log(pessoa1.sobrenome);
+console.log();
+
+
+const pessoa2 = {
+
+    // Atributos
+    nome: 'Maria',
+    sobrenome: 'Oliveira',
+    idade: 55
+};
+
+console.log(pessoa2.nome);
+console.log(pessoa2.sobrenome);
+console.log();
+
+
+// Criando objetos com funções: factory function
+function criaPessoa(nome, sobrenome, idade) {
+
+    return {
+
+        nome,
+        sobrenome,
+        idade
+    };
+}
+
+const pessoa3 = criaPessoa('Erick', 'F. da Silva', 36);
+
+console.log(pessoa3);
+console.log(pessoa3.nome);
+console.log(pessoa3.sobrenome);
+console.log();
