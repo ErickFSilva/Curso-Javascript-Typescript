8963c34d97d0bebbf2eb73cdd4d80dc8c444b827
ErickFSilva <erick_ferreira@outlook.com>
2023-12-27 08:01:28 -0300

    secao-02-javascript-basico, 27/12/2023 (manhã)

---
+++ secao-02_javascript_basico/aula-013-variaveis-com-let.js
@@ -0,0 +1,26 @@
+// Regras de variáveis:
+
+/**
+ * Não podemos criar variáveis com palavras reservadas;
+ * Variáveis precisam ter nomes significativos;
+ * Não pode começar o nome de uma variável com um número;
+ * Variáveis não podem conter espaços ou traços;
+ * Utilizamos camelCase;
+ * Não podemos redeclarar variáveis com 'let';
+ * Crie variáveis, preferencialmente, com 'let';
+ */
+
+let esposo = 'João';
+let esposa = 'Maria';
+let dataCasamento = '2012';
+let dataNasciEsposo = '1984';
+let dataNasciFilho = '2015';
+let filho;
+
+filho = 'Eduardo';
+
+console.log(esposo, `nasceu em ${dataNasciEsposo}.`);
+console.log(`Em 2000 ${esposo} conheceu ${esposa}.`);
+console.log(`${esposo} casou-se com ${esposa} em ${dataCasamento}.`);
+console.log(`${esposa} teve 1 filho com ${esposo} em ${dataNasciFilho}.`);
+console.log(`O filho de ${esposa} com ${esposo} se chama ${filho}.`);
---
+++ secao-02_javascript_basico/aula-014-contantes-com-const.js
@@ -0,0 +1,25 @@
+// Regras de constantes:
+
+/**
+ * Não podemos criar constantes com palavras reservadas;
+ * Constantes precisam ter nomes significativos;
+ * Não pode começar o nome de uma constante com um número;
+ * Constantes não podem conter espaços ou traços;
+ * Utilizamos camelCase;
+ * Case-sensitive;
+ * Não podemos modificar o valor de uma constante;
+ * Crie constantes, preferencialmente, com 'const';
+ */
+
+
+// Constante do tipo string:
+const nome = 'Erick';
+console.log(nome);
+
+// Constante do tipo number:
+const anoAtual = 2023;
+console.log(anoAtual);
+
+// Verifica o tipo do dado recebido:
+console.log(`Tipo da constante: ${typeof nome}`);
+console.log(`Tipo da constante: ${typeof anoAtual}`);
