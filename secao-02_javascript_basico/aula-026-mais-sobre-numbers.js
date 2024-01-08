34c211855ec6b388c802c45730e572df42b554f2
ErickFSilva <erick_ferreira@outlook.com>
2024-01-07 23:04:10 -0300

    secao-02-javascript-basico, 07/01/2024 (noite)

--- secao-02_javascript_basico/aula-026-mais-sobre-numbers.js
+++ secao-02_javascript_basico/aula-026-mais-sobre-numbers.js
@@ -7,27 +7,30 @@ let num2 = 2.5;
 let num3 = 10.57956
 
 // Ler um número como se fosse um string: .toString()
-console.log(num_str.toString() + num2); // String em uma operação de adição é realizado uma concatenação
+console.log(`Operação com string: ${num_str.toString() + num2}`); // String em uma operação de adição é realizado uma concatenação
 
 // Converter um número em string
 num_str = num_str.toString();
-console.log(num_str, ':', typeof num_str);
+console.log(`String: ${num_str}`);
 
 // Representação binária com .toString()
-console.log(num1.toString());
-console.log(num1.toString(2));
+console.log(`Binários: ${num1.toString()}`);
+console.log(`Binários: ${num1.toString(2)}`);
 
 // Arredondando um valor
-console.log(num3.toFixed(2));
+console.log(`Valor arredondado: ${num3.toFixed(2)}`);
 
 // Verifica se o número é inteiro ou não
-console.log(Number.isInteger(num1));
-console.log(Number.isInteger(num_str));
+console.log(`Número inteiro: ${Number.isInteger(num1)}`);
+console.log(`Número inteiro: ${Number.isInteger(num_str)}`);
 
 // Verificar se o resultado é um NaN
-console.log(Number.isNaN(num_NaN));
+console.log(`NaN: ${Number.isNaN(num_NaN)}`);
 
 // Corrigindo bug do IEEE 754-2008
-console.log(ieee1 + ieee2);
+ieee1 += ieee2;
+ieee1 += ieee2;
+ieee1 += ieee2;
 
-// 
+console.log(`Variável "ieee1": ${ieee1}`);
+console.log(`Bug IEEE corrigido: ${ieee1.toFixed(1)}`);
