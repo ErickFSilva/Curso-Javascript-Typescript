768dd7ec4070ba8128940db82bdd97c8bfce67be
ErickFSilva <erick_ferreira@outlook.com>
2024-01-09 10:53:08 -0300

    secao-02-javascript-basico (aula027 incompleta), 09/01/2024 (manhã)

---
+++ secao-02_javascript_basico/aula027-objeto-math.js
@@ -0,0 +1,19 @@
+// - Objeto Math
+
+// Variáveis
+let num1 = 9.5738;
+let num2;
+
+console.log(`Valor real: ${num1}`);
+
+// Arredondando para baixo
+num2 = Math.floor(num1);
+console.log(`Arredondando para baixo: ${num2}`);
+
+// Arredonda para cima
+num2 = Math.ceil(num1);
+console.log(`Arredondando para cima: ${num2}`);
+
+// Arredonda para o mais próximo
+num2 = Math.round(num1);
+console.log(`Arredonda para o mais próximo: ${Math.round(num1)}`);
