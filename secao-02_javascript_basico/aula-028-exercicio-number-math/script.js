30dd458aa0bb56149e41147e1c3cfe160a6cb991
ErickFSilva <erick_ferreira@outlook.com>
2024-04-03 15:46:48 -0300

    Curso Javascript e Typescript - 03/04/2024 (tarde)

--- secao-02_javascript_basico/aula-028-exercicio-number-math/script.js
+++ secao-02_javascript_basico/aula-028-exercicio-number-math/script.js
@@ -3,6 +3,12 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
+
 texto.innerHTML = `
-    <p>Seu número + 2 é ${numero + 2};</p>
+    <p>Raiz quadrada: ${Math.sqrt(numero)}</p>
+    <p>${numero} é inteiro: ${Number.isInteger(numero) ? 'Sim' : 'Não'}</p>
+    <p>É NaN: ${Number.isNaN(numero) ? 'Sim' : 'Não'}</p>
+    <p>Arredondando para baixo: ${Math.floor(numero)}</p>
+    <p>Arredondando para cima: ${Math.ceil(numero)}</p>
+    <p>Com duas casas decimais: ${numero.toFixed(2)}</p>
 `;
