f3f685d415c9e444e32fc24b816d469b8acf0094
ErickFSilva <erick_ferreira@outlook.com>
2024-04-06 11:36:37 -0300

    Curso Javascript e Typescript - 06/04/2024 (manhã)

--- secao-02_javascript_basico/aula-031-funcoes.js
+++ secao-02_javascript_basico/aula-031-funcoes.js
@@ -1,14 +1,14 @@
 // Funções executam ações;
-// 
+
+// Declaração clássica de funções
 function saudacao(nome) {
 
-    // console.log(`Bom dia ${nome}!`);
     return `Bom dia ${nome}!`;
 }
 
 const variavel = saudacao('Luiz');
 console.log(variavel);
-
+console.log();
 
 // 
 function soma(x, y) {
@@ -19,6 +19,7 @@ function soma(x, y) {
 
 let resultado = soma(2, 2);
 console.log(resultado);
+console.log();
 
 
 // Função com valor padrão
@@ -30,3 +31,36 @@ function soma2(x = 1, y = 1) {
 
 resultado = soma2();
 console.log(resultado);
+console.log();
+
+
+// Declaração de função anônima
+const raiz = function (n) {
+
+    return Math.sqrt(n); // (Raiz quadrada)
+};
+
+console.log(raiz(9));
+console.log();
+
+
+// Arrow functions - modelo 1
+const arrowFunc1 = (n) => {
+
+    return n ** 0.5; // (Raiz quadrada)
+};
+
+console.log(arrowFunc1(16));
+console.log();
+
+// Arrow functions - Modelo 2 (Tendo só a linha de retorno)
+const arrowFunc2 = (n) => n ** 0.5; // (Raiz quadrada)
+
+console.log(arrowFunc2(25));
+console.log();
+
+// Arrow functions - Modelo 3 (Tendo só um parâmetro)
+const arrowFunc3 = n => Math.sqrt(n); // (Raiz quadrada)
+
+console.log(arrowFunc3(36));
+console.log();
---
+++ secao-02_javascript_basico/aula-032-objetos.js
@@ -0,0 +1,46 @@
+// Criando um objeto simples
+const objetoPessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25
+};
+
+console.log(objetoPessoa.nome);
+console.log();
+
+
+// Criando um função para criar objetos, 'facture function' (fábrica de objetos)
+function criaPessoa (nome, sobrenome, idade) {
+
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    }
+}
+
+const pessoa1 = criaPessoa('Erick', 'Ferreira', 37);
+
+console.log(pessoa1.nome, pessoa1.sobrenome, pessoa1.idade);
+console.log();
+
+
+// Quando uma 'função' esté DENTRO DE UM OBJETO ela é chamada de 'método'
+const pessoa2 = {
+    nome: 'Erick',
+    sobrenome: 'Ferreira',
+    idade: 37,
+
+    falar() {
+        console.log(`Eu, ${this.nome} ${this.sobrenome}, tenho ${this.idade} anos`);
+        console.log();
+    },
+
+    incrementaIdade() {
+        this.idade++;
+    }
+}
+
+pessoa2.falar();
+pessoa2.incrementaIdade();
+pessoa2.falar();
