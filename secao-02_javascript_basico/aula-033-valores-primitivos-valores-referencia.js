234a195652831ac20c6acd7e824ddefe9e5a9ed6
ErickFSilva <erick_ferreira@outlook.com>
2024-04-08 16:42:29 -0300

    Curso Javascript e Typescript - 08/04/2024 (tarde)

---
+++ secao-02_javascript_basico/aula-033-valores-primitivos-valores-referencia.js
@@ -0,0 +1,12 @@
+/**
+ * - Primitivos (imutáveis): string, number, boolean, undefined, null (bigint, symbol)
+ * 
+ * - Referência (mutáveis): array, object, function
+ */
+
+let tipo = 'Luiz';
+tipo = 2024;
+console.log(tipo);
+
+tipo = ['Luiz'];
+console.log(tipo);
