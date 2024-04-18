659abe54dad9fce339f948da65c72b5fdf32049f
Erick Ferreira da Silva <erick.silva@jfpe.jus.br>
2024-04-18 15:51:26 -0300

    Curso Javascript e Typescript - 18/04/2024 (tarde)

--- secao-03_javascript-logica-programacao/aula-043-exercicio-imc/assets/js/main.js
+++ secao-03_javascript-logica-programacao/aula-043-exercicio-imc/assets/js/main.js
@@ -1,14 +1,60 @@
 // Capturar evento de submit do formulário
-const form = document.querySelector('#formulario');
+const form = document.querySelector('#form');
+let resultado, imc, peso, altura, msg;
 
+
+// Captura os dados
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    console.log('Evento prevenido');
-    setResultado('Evento prevenido');
+    peso = form.querySelector('#peso').value;
+    altura = form.querySelector('#altura').value;
+
+    setResultado(precessaDados());
 });
 
+
+// Processa dados
+function precessaDados() {
+
+    imc = peso / (altura * altura);
+
+    if (imc < 18.5) {
+        msg = `IMC ${imc.toFixed(1)}. Abaixo do peso`;
+    }
+    else if (imc <= 24.9) {
+        msg = `IMC ${imc.toFixed(1)}. Peso normal`;
+    }
+    else if (imc <= 29.9) {
+        msg = `IMC ${imc.toFixed(1)}. Sobrepeso`;
+    }
+    else if (imc <= 34.9) {
+        msg = `IMC ${imc.toFixed(1)}. Obesidade grau 1`;
+    }
+    else if (imc <= 39.9) {
+        msg = `IMC ${imc.toFixed(1)}. Obesidade grau 2`;
+    }
+    else if (imc >= 40) {
+        msg = `IMC ${imc.toFixed(1)}. Obesidade grau 3`;
+    }
+
+    return msg;
+
+}
+
+function criaP(msg) {
+    const p = document.createElement('p');
+    p.classList.add('resultado');
+    p.innerHTML = msg;
+
+    return p;
+}
+
+
+// Envia os dados
 function setResultado (msg) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = `<span class="resul-color">${msg}</span>`;
+    const resultado = document.querySelector('#resultado-imc');
+
+    resultado.innerHTML = '';
+    resultado.appendChild(criaP(msg));
 }
